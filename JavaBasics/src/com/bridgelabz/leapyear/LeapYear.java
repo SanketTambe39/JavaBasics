a24c97eb87ac29bfd1e96cc9d922a5102a4de2b4
SanketTambe39 <sankettambe39@gmail.com>
2021-06-23 21:15:02 +0530

    ADDED: Code for checking leap year or not

---
+++ JavaBasics/src/com/bridgelabz/leapyear/LeapYear.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.leapyear;
+
+import java.util.Scanner;
+/**
+ * class to check that the given year is a leap year or not
+ * @author Sanket
+ *
+ */
+public class LeapYear {
+
+	public static void main(String[] args) {
+		
+		int year;
+		
+		System.out.println("Enter year");
+		Scanner sc=new Scanner(System.in);
+		year=sc.nextInt();
+		sc.close();
+		
+		checkLeapYear(year);
+
+	}
+/**
+ * Name:checkLeapYear
+ * 
+ * Description: method to check given year is leap year or not
+ * 
+ * @param year
+ * 
+ * Modification: Added year 1582 from where the Gregorian calendar calender started
+ * 
+ */
+	private static void checkLeapYear(int year) {
+		if (year >= 1582) {
+			if(((year%4==0)&&(year%100!=0))||(year%400==0))
+			{
+				System.out.println("You Just Enter A Leap Year "+year);
+			}else
+			{
+				System.out.println("You Just Enter Not A Leap Year "+year);
+			}
+			
+		} else {
+
+				System.out.println("Entered a year which is not in the Gregorian calendar");
+			
+		}
+	}
+
+}
